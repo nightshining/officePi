5e54e2ea0d9bb44272230e3070e88fdf01205097
Mike Romeo <michaelpromeo@gmail.com>
2015-09-04 12:12:08 -0400

    @nightshining adding shader example

---
+++ Shader/shaderExample/src/ofApp.cpp
@@ -0,0 +1,85 @@
+// Copyright 2015 Patricio Gonzalez Vivo (http://patriciogonzalezvivo.com)
+
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    //  Load and compile the shader
+    //
+    shader.load("","shader.frag");
+    ofSetCircleResolution(60);
+    ofBackground(0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    // Replace the pipeline with our shader
+    shader.begin();
+    
+    // Send uniforms
+    shader.setUniform1f("u_time", ofGetElapsedTimef());
+    shader.setUniform2f("u_mouse", mouseX, mouseY);
+    shader.setUniform2f("u_resolution", ofGetWidth(), ofGetHeight());
+   
+    // make a billboard
+    ofCircle(ofGetWidth() * .5, ofGetHeight() * .5, 250);
+    // Default shader pipeline
+    shader.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+    //  Reload everytime you press a key
+    //
+    shader.load("","shader.frag");
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
